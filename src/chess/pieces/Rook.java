2001d9f6f37ec5daf6b5a2a942975fc1a77353fa
Lucas Soares <drak263@gmail.com>
2023-01-28 16:07:08 -0300

    Placing pieces on the board

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+/**
+ *
+ * @author lucas
+ */
+public class Rook extends ChessPiece {
+    
+    public Rook(Color color, Board board) {
+        super(color, board);
+    }
+    
+    @Override
+    public String toString() {
+        return "R";
+    }
+}
