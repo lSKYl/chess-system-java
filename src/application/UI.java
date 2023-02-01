fe1d5ee4e2c6900b3e0f098deadd475e409ffc40
Lucas Soares <drak263@gmail.com>
2023-02-01 08:55:43 -0300

    Switching player

--- src/application/UI.java
+++ src/application/UI.java
@@ -4,6 +4,7 @@
  */
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -41,6 +42,13 @@ public class UI {
         System.out.flush();
     }
 
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    }
+    
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
--- src/application/program.java
+++ src/application/program.java
@@ -29,7 +29,7 @@ public class program {
         try {
             while (true) {
                 UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
